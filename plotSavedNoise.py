ce69209f9d645a9d41b15acab3f49d1a0ce54157
jed4h <jed4h@virginia.edu>
2015-05-28 15:01:26 -0400

    Create plotSavedNoise.py

---
+++ plotSavedNoise.py
@@ -0,0 +1,24 @@
+# produces a time series of noise level data from a processed file
+# Noise is passed through a high pass filter on the BBB
+
+def plotNoise(inFile):
+    time_data = []
+    noise_data = []
+    
+    # read starting time and date
+    inFile.readline()
+    
+    # ignore line with metadata 
+    inFile.readline()
+    
+    for line in inFile:
+        splitData = line.split(",")
+        #print splitData
+        try:
+            time_data.append(float(splitData[0]))
+            noise_data.append(float(splitData[1]))
+        except:
+            print "error processing float"
+
+        
+    return time_data, noise_data
